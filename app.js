4ff10ee948fe6980623a8995efb427025034bbbd
Varun <64461219+varun-deokar@users.noreply.github.com>
2021-05-05 00:57:15 +0530

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app     = express()
+var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + "/public"))
@@ -8,6 +9,6 @@ app.get('/', (req, res) => {
     res.render('ColourPicker')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Listening on port 3000')
 })
